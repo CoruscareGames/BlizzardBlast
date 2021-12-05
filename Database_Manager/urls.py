2c2f821b4ce03177032e4e370ec2fd96be1e1e02
CaelebC <caeleb.camaro@obf.ateneo.edu>
2021-12-05 17:13:27 +0800

    Added pages for different aspects of the project

--- Database_Manager/urls.py
+++ Database_Manager/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='manage_home'),
+    path('inventory/', views.inventory, name='manage_inventory'),
+    path('recipes/', views.recipes, name='manage_recipes'),
+    path('employees/', views.employees, name='manage_employees'),
+    path('schedule/', views.schedule, name='manage_schedule'),
+    path('sales/', views.sales, name='manage_sales'),
 ]
--- Database_Manager/views.py
+++ Database_Manager/views.py
@@ -4,3 +4,18 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse('Homepage')
+
+def inventory(request):
+    return HttpResponse('Inventory Page')
+
+def recipes(request):
+    return HttpResponse('Base Recipes Page')
+
+def employees(request):
+    return HttpResponse('Employees Page')
+
+def schedule(request):
+    return HttpResponse('Schedule Page')
+
+def sales(request):
+    return HttpResponse('Past Sales Page')
