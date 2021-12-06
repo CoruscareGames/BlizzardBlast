6022f394224d49397bc707b243cfa338a10a69da
CaelebC <caeleb.camaro@obf.ateneo.edu>
2021-12-06 23:00:25 +0800

    Created a working "new_sale" form. Created a list of "sales". Cleaned up UI slightly.

--- Database_Manager/urls.py
+++ Database_Manager/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='manage_home'),
+    path('', views.new_sale, name='manage_home'),
     path('inventory/', views.inventory, name='manage_inventory'),
     path('recipes/', views.recipes, name='manage_recipes'),
     path('employees/', views.employees, name='manage_employees'),
--- Database_Manager/views.py
+++ Database_Manager/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import *
+from .forms import *
 
 # Create your views here.
-def home(request):
-    return render(request, 'Database_Manager/new_sale.html')
+def new_sale(request):
+    form = OrderForm
+    context = {
+        'form': form, 
+    }
+    
+    if request.method == "POST":
+        form = OrderForm(request.POST)
+        
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+            
+    return render(request, 'Database_Manager/new_sale.html', context)
 
 def inventory(request):
     return render(request, 'Database_Manager/inventory.html')
@@ -18,4 +32,8 @@ def schedule(request):
     return render(request, 'Database_Manager/schedule.html')
 
 def sales_list(request):
-    return render(request, 'Database_Manager/sales_list.html')
+    context = {
+        'sales': OrderSample.objects.all()
+    }
+    return render(request, 'Database_Manager/sales_list.html', context)
+    
