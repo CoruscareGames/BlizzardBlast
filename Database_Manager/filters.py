cc5d9bf6c9a49211e4198fc00fc5085a505e83f1
CaelebC <caeleb.camaro@obf.ateneo.edu>
2021-12-12 22:55:53 +0800

    Created a dropdown for filtering date range. Created a filter for customer name.

--- Database_Manager/filters.py
+++ Database_Manager/filters.py
@@ -1,7 +1,12 @@
 import django_filters
+from django_filters import DateFilter, CharFilter, DateRangeFilter
 from .models import *
 
 class SaleFilter(django_filters.FilterSet):
+    customer = CharFilter(field_name='customer_name', lookup_expr='icontains')
+    range = DateRangeFilter(field_name='day_date')
+
     class Meta:
         model = Sale
-        fields = ['day_date', 'week_date', ]
+        fields = []
+        
